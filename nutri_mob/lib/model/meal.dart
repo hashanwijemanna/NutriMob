8da10163eab96e2b397e73b1fae3e66667df75d7
RAWINI SATHEESHA <rsatheeshakaradanvers@gmail.com>
2024-09-01 15:17:17 +0530

    xxx

---
+++ nutri_mob/lib/model/meal.dart
@@ -0,0 +1,15 @@
+class Meal {
+  final String mealTime, name, imagePath, kiloCaloriesBurnt, timeTaken;
+
+  Meal ( {
+    this.mealTime,
+    this.name,
+    this.imagePath,
+    this.kiloCaloriesBurnt,
+    this.timeTaken,
+});
+}
+final meals = [ Meal(
+  mealTime: "Beakfast",
+
+)]
---
+++ nutri_mob/lib/ui/profile screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   final height = MediaQuery.of(context).size.height;
+
+   return Scaffold(
+     backgroundColor: const Color(0xFFE9E9E9),
+     bottomNavigationBar:  ClipRect(
+       borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+     child: BottomNavigationBar(
+       iconSize: 40,
+         selectedIconTheme: IconThemeData(
+           color: const Color(0xff200087),
+         ),
+         unselectedIconTheme: IconThemeData(
+           color: const Colors.black12,
+         ),
+
+
+         items: [
+           BottomNavigationBarItem(
+     icon: Padding(
+     padding: const EdgeInsets.only(top: 8.0),
+             child: Icon(Icon.home),
+     ),
+             title: Text(
+               "Home",
+               style: const TextStyle(color: Colors.white),
+             ),
+           ),
+
+
+           BottomNavigationBarItem(
+             icon: Padding(
+           child: Icon(Icons.search),
+    padding: const EdgeInsets.only(top: 8.0),
+    ),
+             title: Text(
+               "Search",
+               style: const TextStyle(color: Colors.white),
+             ),
+           ),
+
+
+           BottomNavigationBarItem(
+             icon: Padding(
+    child: Icon(Icon.person),
+    padding: const EdgeInsets.only(top: 8.0),
+    ),
+             title: Text(
+               "Profile",
+               style: const TextStyle(color: Colors.white),
+             ),
+           ),
+
+         ],
+     ),
+     body: Stack(
+       children: <Widget>[
+         Positioned(
+           top: 0,
+           height: height * 0.35,
+           left: 0,
+           right: 0,
+           child: ClipRect(
+             borderRadius: const BorderRadius.vertical(
+               bottom: const Radius.circular(40),
+             ),
+             child: Container(
+               color: Colors.white,
+             ),
+           ),
+         ),
+    Positioned(
+    top: height * 0.38,
+    left: 0,
+    right: 0,
+    child: Container(
+
+    height: height,
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Padding(
+    padding: const EdgeInsets.only(
+        bottom: 8,
+    left: 32,
+    right: 16,
+    ),
+
+    child: Text(
+    "MEALS FOR TODAY",
+    style: const TextStyle(
+    color: Colors.blueGrey,
+    fontSize: 16,
+    fontWeight: FontWeight.w700
+    ),
+    ),
+    ),
+    Expanded(
+    child: SingleChildScrollView(
+    child: Row(
+    children: <Widget>[
+
+    ],
+    ),
+    ),
+    ),
+
+    Expanded(
+    child: Container(
+    color: Colors.redAccent,
+    child: ,
+    ),
+    ),
+
+    Expanded(
+    child: Container(
+    color: Colors.blueAccent,
+
+    ),
+    ),
+
+
+
+    ],
+    ),
+    ),
+    ),
+       ]
+     ),
+   );
+  }
+}
