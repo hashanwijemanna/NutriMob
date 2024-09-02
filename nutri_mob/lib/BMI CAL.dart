e953cf2e3a738c21976aadbcb00dc24947a7fa78
Thamal <thamalrcg@gmail.com>
2024-09-02 15:06:04 +0530

    sliding pages
    wkwkwk

--- nutri_mob/lib/BMI CAL.dart
+++ nutri_mob/lib/BMI CAL.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:nutri_mob/home.dart';
-import 'package:nutri_mob/main.dart';
 
 class BMICalculater extends StatefulWidget {
   const BMICalculater({Key? key}) : super(key: key);
@@ -19,7 +18,7 @@ class _BMICalculaterState extends State<BMICalculater> {
   var foodItemsController = TextEditingController();
 
   var result = "";
-  var bgColor = Colors.indigo.shade200;
+  var bgColor = Colors.blueGrey.shade100;
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseDatabase _database = FirebaseDatabase.instance;
@@ -29,151 +28,172 @@ class _BMICalculaterState extends State<BMICalculater> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '',
+          'BMI Calculator',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: bgColor,
+        backgroundColor: Colors.blueGrey.shade800,
         iconTheme: IconThemeData(color: Colors.white), // Back button color
       ),
       body: Container(
         color: bgColor,
-        child: Center(
-          child: Container(
-            width: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Enter your details',
-                  style: TextStyle(
-                    fontSize: 34,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: double.infinity,
+              constraints: BoxConstraints(maxWidth: 500),
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
                   ),
-                ),
-                SizedBox(height: 21),
-                TextField(
-                  controller: wtController,
-                  decoration: InputDecoration(
-                    label: Text('Enter your Weight (in Kgs)'),
-                    prefixIcon: Icon(Icons.line_weight),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-                TextField(
-                  controller: htController,
-                  decoration: InputDecoration(
-                    label: Text('Enter your Height (in centimeters)'),
-                    prefixIcon: Icon(Icons.height),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  controller: agController,
-                  decoration: InputDecoration(
-                    label: Text('Enter your Age'),
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  controller: allergiesController,
-                  decoration: InputDecoration(
-                    label: Text('Enter Allergies (if any)'),
-                    prefixIcon: Icon(Icons.warning),
-                  ),
-                  keyboardType: TextInputType.text,
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  controller: foodItemsController,
-                  decoration: InputDecoration(
-                    label: Text('Food Items Recommended by Doctors (if any)'),
-                    prefixIcon: Icon(Icons.local_dining),
-                  ),
-                  keyboardType: TextInputType.text,
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () async {
-                    var wt = wtController.text.toString();
-                    var ht = htController.text.toString();
-                    var age = agController.text.toString();
-                    var allergies = allergiesController.text.toString();
-                    var foodItems = foodItemsController.text.toString();
-
-                    if (wt != "" && ht != "" && age != "") {
-                      // BMI CALCULATION
-                      var iWt = int.parse(wt);
-                      var iht = int.parse(ht);
-                      var iAge = int.parse(age);
-
-                      var tM = iht / 100;
-                      var bmi = iWt / (tM * tM);
-
-                      // Get the current user
-                      User? user = _auth.currentUser;
-                      if (user != null) {
-                        String email = user.email ?? "unknown";
-
-                        // Prepare data to push to Firebase Realtime Database
-                        Map<String, dynamic> userData = {
-                          "weight": iWt,
-                          "height": iht,
-                          "age": iAge,
-                          "bmi": bmi.toStringAsFixed(2),  // Push BMI value
-                          "email": email,
-                        };
-
-                        // Add optional fields if they are not empty
-                        if (allergies.isNotEmpty) {
-                          userData["allergies"] = allergies;
-                        }
-                        if (foodItems.isNotEmpty) {
-                          userData["food_items_recommended"] = foodItems;
-                        }
-
-                        // Push data to Firebase Realtime Database
-                        await _database.ref("users/${user.uid}/bioData").set(userData);
-
-                        // Navigate to the result page if successful
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HomeScreen(),
-                          ),
-                        );
-                      }
-                    } else {
-                      setState(() {
-                        result = "Please fill all the required fields!";
-                      });
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.green, // Button color
-                    textStyle: TextStyle(
-                      fontWeight: FontWeight.bold, // Text bold
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Enter your details',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.blueGrey.shade800,
                     ),
                   ),
-                  child: Text('Submit'),
-                ),
-                SizedBox(height: 11),
-                Text(
-                  result,
-                  style: TextStyle(
-                    fontSize: 19,
-                    color: Colors.red, // Result text color
+                  SizedBox(height: 20),
+                  _buildTextField(
+                    controller: wtController,
+                    label: 'Weight (in Kgs)',
+                    icon: Icons.line_weight,
                   ),
-                ),
-              ],
+                  SizedBox(height: 16),
+                  _buildTextField(
+                    controller: htController,
+                    label: 'Height (in centimeters)',
+                    icon: Icons.height,
+                  ),
+                  SizedBox(height: 16),
+                  _buildTextField(
+                    controller: agController,
+                    label: 'Age',
+                    icon: Icons.person,
+                  ),
+                  SizedBox(height: 16),
+                  _buildTextField(
+                    controller: allergiesController,
+                    label: 'Allergies (if any)',
+                    icon: Icons.warning,
+                  ),
+                  SizedBox(height: 16),
+                  _buildTextField(
+                    controller: foodItemsController,
+                    label: 'Food Items Recommended (if any)',
+                    icon: Icons.local_dining,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      var wt = wtController.text.toString();
+                      var ht = htController.text.toString();
+                      var age = agController.text.toString();
+                      var allergies = allergiesController.text.toString();
+                      var foodItems = foodItemsController.text.toString();
+
+                      if (wt.isNotEmpty && ht.isNotEmpty && age.isNotEmpty) {
+                        var iWt = int.parse(wt);
+                        var iht = int.parse(ht);
+                        var iAge = int.parse(age);
+
+                        var tM = iht / 100;
+                        var bmi = iWt / (tM * tM);
+
+                        User? user = _auth.currentUser;
+                        if (user != null) {
+                          String email = user.email ?? "unknown";
+
+                          Map<String, dynamic> userData = {
+                            "weight": iWt,
+                            "height": iht,
+                            "age": iAge,
+                            "bmi": bmi.toStringAsFixed(2),
+                            "email": email,
+                          };
+
+                          if (allergies.isNotEmpty) {
+                            userData["allergies"] = allergies;
+                          }
+                          if (foodItems.isNotEmpty) {
+                            userData["food_items_recommended"] = foodItems;
+                          }
+
+                          await _database.ref("users/${user.uid}/bioData").set(userData);
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        }
+                      } else {
+                        setState(() {
+                          result = "Please fill all the required fields!";
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green, // Button color
+                      foregroundColor: Colors.white, // Text color
+                      padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 24.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text('Submit'),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    result,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+  }) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(icon, color: Colors.blueGrey.shade600),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: BorderSide(color: Colors.blueGrey.shade300),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: BorderSide(color: Colors.blueGrey.shade600),
+        ),
+      ),
+      keyboardType: TextInputType.text,
     );
   }
 }
