b0a25e4ebd1c995cec26edbc81222dbc6b3263bd
RAWINI SATHEESHA <rsatheeshakaradanvers@gmail.com>
2024-09-06 21:43:04 +0530

    2w

--- nutri_mob/lib/ui/condition_page.dart
+++ nutri_mob/lib/ui/condition_page.dart
@@ -8,13 +8,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ConditionPage(),
+      home: DashboardPage(),
       theme: ThemeData(
         primarySwatch: Colors.teal,
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           titleLarge: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
           bodyMedium: TextStyle(fontSize: 16, color: Colors.black87),
         ),
+      ),
+    );
+  }
+}
+
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _currentIndex = 0;
+
+  // Define different pages (widgets) for dashboard items
+  final List<Widget> _pages = [
+    HomePage(),
+    ConditionPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        backgroundColor: Colors.teal,
+      ),
+      body: _pages[_currentIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        backgroundColor: Colors.teal,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_circle),
+            label: 'Conditions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Home Page',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Profile Page',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
     );
   }
@@ -32,94 +108,87 @@ class _ConditionPageState extends State<ConditionPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Conditions"),
-        backgroundColor: Colors.teal,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: Text(
-                'Before You Proceed',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal,
-                ),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Text(
+              'Before You Proceed',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal,
               ),
             ),
-            SizedBox(height: 10),
-            Center(
-              child: Text(
-                'Please accept the following conditions:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.black54,
-                ),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              'Please accept the following conditions:',
+              style: TextStyle(
+                fontSize: 18,
+                fontStyle: FontStyle.italic,
+                color: Colors.black54,
               ),
             ),
-            SizedBox(height: 30),
-            _buildCustomCheckbox(
-              title: "I am aware of my health and concerned about my well-being.",
-              value: _isHealthAwarenessChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isHealthAwarenessChecked = value ?? false;
-                });
-              },
-            ),
-            _buildCustomCheckbox(
-              title: "I agree to provide my true information and data.",
-              value: _isTrueInformationChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isTrueInformationChecked = value ?? false;
-                });
-              },
-            ),
-            _buildCustomCheckbox(
-              title: "I accept the rules and regulations provided by the app.",
-              value: _isTermsAndConditionsChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isTermsAndConditionsChecked = value ?? false;
-                });
-              },
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: ElevatedButton(
-                onPressed: _isHealthAwarenessChecked &&
-                    _isTrueInformationChecked &&
-                    _isTermsAndConditionsChecked
-                    ? () {
-                  // Handle button press
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("All conditions accepted!"),
-                      backgroundColor: Colors.green,
-                    ),
-                  );
-                }
-                    : null, // Disable button if conditions are not met
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: Colors.teal, // Text color
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                  textStyle: TextStyle(fontSize: 18),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+          ),
+          SizedBox(height: 30),
+          _buildCustomCheckbox(
+            title: "I am aware of my health and concerned about my well-being.",
+            value: _isHealthAwarenessChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                _isHealthAwarenessChecked = value ?? false;
+              });
+            },
+          ),
+          _buildCustomCheckbox(
+            title: "I agree to provide my true information and data.",
+            value: _isTrueInformationChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                _isTrueInformationChecked = value ?? false;
+              });
+            },
+          ),
+          _buildCustomCheckbox(
+            title: "I accept the rules and regulations provided by the app.",
+            value: _isTermsAndConditionsChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                _isTermsAndConditionsChecked = value ?? false;
+              });
+            },
+          ),
+          SizedBox(height: 30),
+          Center(
+            child: ElevatedButton(
+              onPressed: _isHealthAwarenessChecked &&
+                  _isTrueInformationChecked &&
+                  _isTermsAndConditionsChecked
+                  ? () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("All conditions accepted!"),
+                    backgroundColor: Colors.green,
                   ),
+                );
+              }
+                  : null,
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white, backgroundColor: Colors.teal,
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                textStyle: TextStyle(fontSize: 18),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                child: Text('Continue'),
               ),
+              child: Text('Continue'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
